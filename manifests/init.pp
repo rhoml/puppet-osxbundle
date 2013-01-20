c2dcb8f00975af983b5c79ac59120b88be4374fb
rhoml <roml@rhommell.com>
2013-01-20 19:19:16 +0100

    Added more packages.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,15 @@
 class xcode {
 
   package { 'xcode_4.4.1_6938145.dmg':
-    provider  => pkgdmg,
-    ensure    => present,
+    provider  => 'pkgdmg',
+    ensure    => 'installed',
     source    => 'http://djmgsl5zm17hq.cloudfront.net/thesetup/xcode/xcode_4.4.1_6938145.dmg'
   }
+
+  package { 'AdobeAIR.dmg':
+    ensure    => 'installed',
+    provider  => 'pkgdmg',
+    source    => 'http://djmgsl5zm17hq.cloudfront.net/thesetup/AdobeAir/AdobeAIR.dmg'
+  }
+
 }
