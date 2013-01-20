d88c18004753a5ba58227588dd4686e429cb366a
rhoml <roml@rhommell.com>
2013-01-20 18:57:20 +0100

    Intentional space

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
+#== Class nginx
 class xcode {
 
   package { 'xcode_4.4.1_6938145.dmg':
-    provider => pkgdmg, ensure => present
-    source => 'http://djmgsl5zm17hq.cloudfront.net/thesetup/xcode/xcode_4.4.1_6938145.dmg',
+    provider  => pkgdmg,
+    ensure    => present,
+    source    => 'http://djmgsl5zm17hq.cloudfront.net/thesetup/xcode/xcode_4.4.1_6938145.dmg'
   }
 }
